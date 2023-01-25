9f67aea980dae3eea8bd9df8b4a94f4ae1024848
chenyihe <645695312@qq.com>
2023-01-25 11:11:40 +0800

    feat: alert update

--- src/Alert/demos/types.tsx
+++ src/Alert/demos/types.tsx
@@ -6,6 +6,7 @@ export default () => (
     <Alert description="简单警示"></Alert>
     <Alert description="成功警示" type='success'></Alert>
     <Alert description="信息警示" type='info'></Alert>
+    <Alert description="警告警示" type='warning'></Alert>
     <Alert description="危险危险危险危险！！！！" type='danger'></Alert>
   </div>
 );
