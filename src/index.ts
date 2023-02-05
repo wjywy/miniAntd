4b0d162002491f4342baadc3ef097bcca0adab10
如月十八 <1911834200@qq.com>
2023-02-05 11:29:50 +0800

    fix: 修改了icon alert目录及导入时的拼写,统一成了小写开头

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export { default as Alert } from './Alert';
+export { default as Alert } from './alert';
 export { Avatar, AvatarGroup } from './avatar';
 export { default as Breadcrumb } from './breadcrumb';
 export { default as Button } from './button';
 export { default as Drawer } from './drawer';
 export { default as Foo } from './foo';
 export { default as Icon } from './icon';
+export { default as Switch } from './switch';
