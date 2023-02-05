41b04666204af1ff977ce49c0e4e9d994e057594
chenyihe <645695312@qq.com>
2023-02-05 14:30:33 +0800

    feat: 合并gmy分支

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as Button } from './button';
 export { default as Foo } from './foo';
 export { default as Alert } from './Alert';
 export { default as Icon } from './icon';
-// export { default as Selector } from './select';
+export { default as Selector } from './select';
