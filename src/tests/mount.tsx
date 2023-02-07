1de8904ec09c968a2b4ae6284ed4eedce214b94d
wyx <2603243185@qq.com>
2023-02-08 00:40:57 +0800

    feat(test): test hooks

---
+++ src/tests/mount.tsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+export default function mountTest(Component: React.ComponentType) {
+  describe(`mount and unmount`, () => {
+    it(`component could be updated and unmounted without errors`, () => {
+      const { unmount, rerender } = render(<Component />);
+      expect(() => {
+        rerender(<Component />);
+        unmount();
+      }).not.toThrow();
+    });
+  });
+}
