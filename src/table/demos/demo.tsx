483235451c74a5841457ae492c85aca786f807d1
chenyihe <645695312@qq.com>
2023-02-08 16:58:53 +0800

    feat: simpleTable no css

--- src/table/demos/demo.tsx
+++ src/table/demos/demo.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Table from '../table';
 
 const data = [
-  { name: 'cyh', age: 20, sex: 'male' },
-  { name: 'cyh', age: 20, sex: 'male' },
+  { name: 'cyh', age: 18, sex: 'male' },
+  { name: 'cyh', age: 19, sex: 'male' },
   { name: 'cyh', age: 20, sex: 'male' },
 ];
 
 const header=['name','age','sex']
-export default () => <Table headers={header} data={data}></Table>;
+export default () => <Table headers={header} data={data} sortable={true}></Table>;
